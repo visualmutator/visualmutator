2dd6d08c27c5a4e3092f5981adf226ba24ef57bf
PiotrTrzpil <piotr.trzpil@gmail.com>
2011-07-09 15:24:37 +0200

    init

---
+++ VisualMutator.VSPackage/PkgCmdID.cs
@@ -0,0 +1,13 @@
+﻿// PkgCmdID.cs
+// MUST match PkgCmdID.h
+using System;
+
+namespace PiotrTrzpil.VisualMutator_VSPackage
+{
+    static class PkgCmdIDList
+    {
+
+        public const uint cmdidVisualMutator =    0x101;
+
+    };
+}
