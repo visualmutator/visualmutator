8f4b796d9665f93088b90316c0dbe6c3c0a82166
Piotr Trzpil <devnull@localhost>
2013-06-16 16:55:33 +0200

    generacja start

---
+++ VisualMutator/Model/TestGeneration/TargetAnalizer.cs
@@ -0,0 +1,9 @@
+
+//Analizes the Act phase to get info about parameters or other objects that must be created in Arrage phase.
+//Starting from target method call, it recursively resolves dependencies.
+public class TargetAnalizer
+{
+	NameGenerator gen;
+	
+	
+}
