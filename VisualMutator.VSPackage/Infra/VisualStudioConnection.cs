ccc64ff7df97d18df95d981e01d4023df2d647b2
Piotr Trzpil <piotr.trzpil@expectar.com>
2013-09-05 16:04:11 +0200

    konfiguracja

--- VisualMutator.VSPackage/Infra/VisualStudioConnection.cs
+++ VisualMutator.VSPackage/Infra/VisualStudioConnection.cs
@@ -6,6 +6,7 @@
     using System.Collections.Generic;
     using System.IO;
     using System.Linq;
+    using System.Reactive.Subjects;
     using System.Reflection;
     using CommonUtilityInfrastructure;
     using CommonUtilityInfrastructure.Paths;
